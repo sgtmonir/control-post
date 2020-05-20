e4914a41e17ad9f7d172290a924749323c2acd06
Monir Hossain <sgtmonirh@gmail.com>
2020-05-20 21:32:34 +0600

    First Grid

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+( function( $ ) {
+	// Dynamic Copyright Year
+	$('#year').text(new Date().getFullYear());
+	
+	// Background Img
+	$(".bg-image").each(function() {
+		var bgData = $(this).attr("data-bg-src");
+		$(this).css('background-image', 'url(' +bgData+ ')');
+	});
+
+	//Masonry Init
+	$('.masonry-grid').masonry({});
+
+} )( jQuery );
+
